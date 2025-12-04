9a6b7b377878180b9b4e89a73666eb42e7c7b734
mkski <mikekalaski@gmail.com>
2025-12-04 09:54:06 -0500

    add grid helper, update day 4

--- crates/day04/src/main.rs
+++ crates/day04/src/main.rs
@@ -1,3 +1,4 @@
+use aoc2025::grid::Grid;
 use aoc2025::utils;
 use std::env;
 
@@ -7,68 +8,27 @@ struct Solution {
     part2: i64,
 }
 
-fn neighbor_rolls(grid: &Vec<Vec<char>>, r: usize, c: usize) -> i32 {
-    let neighbors: [(i32, i32); 8] = [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-    let mut count = 0;
-    for (nr, nc) in neighbors.iter() {
-        if let Some(neighbor_row) = grid.get((r as i32 + nr) as usize) {
-            if let Some(neighbor) = neighbor_row.get((c as i32 + nc) as usize) {
-                if *neighbor == '@' {
-                    count += 1;
-                }
-            }
-        }
-    }
-    count
-}
-
-fn remove_rolls(grid: &mut Vec<Vec<char>>) -> i64 {
-    let mut removed = 0;
-    let rows = grid.len();
-    let cols = grid[0].len();
-    let mut to_remove: Vec<(usize, usize)> = Vec::new();
-
-    for r in 0..rows {
-        for c in 0..cols {
-            if grid[r][c] == '@' && neighbor_rolls(grid, r, c) < 4 {
-                to_remove.push((r, c));
-            }
-        }
-    }
-
-    for (r, c) in to_remove {
-        grid[r][c] = '.';
-        removed += 1;
-    }
-
-    removed
-}
-
 fn main() {
     let filename = env::args().nth(1).unwrap();
     let input = utils::read_input(&filename);
-    let mut grid: Vec<Vec<char>> = Vec::new();
     let mut solution = Solution { part1: 0, part2: 0 };
+    let mut grid = Grid::parse(input);
 
-    for line in input.lines() {
-        grid.push(line.chars().collect());
+    let mut iteration = 0;
+    loop {
+        let cells_to_remove = grid.iter_cells().filter(|cell| {
+            let neighbors = grid.count_neighbors_at(cell.row, cell.col, |v| v == '@');
+            cell.value == '@' && neighbors < 4
+        });
+        let removed = grid.update_cells(cells_to_remove.collect(), '.');
+        if iteration == 0 {
+            solution.part1 += removed as i64;
+        }
+        solution.part2 += removed as i64;
+        if removed == 0 {
+            break;
+        }
+        iteration += 1;
     }
-
-    let mut removed = remove_rolls(&mut grid);
-    solution.part1 += removed;
-    while removed > 0 {
-        solution.part2 += removed;
-        removed = remove_rolls(&mut grid);
-    }
-
     println!("{:?}", solution);
 }
---
+++ src/grid.rs
@@ -0,0 +1,84 @@
+use std::fmt::{Display, Formatter};
+
+pub struct GridCell {
+    pub value: char,
+    pub row: usize,
+    pub col: usize,
+}
+
+pub struct Grid {
+    grid: Vec<Vec<char>>,
+}
+
+impl Grid {
+    pub fn parse(input: String) -> Self {
+        let mut grid: Vec<Vec<char>> = Vec::new();
+        for (r, row) in input.lines().enumerate() {
+            grid.push(Vec::new());
+            for (c, col) in row.chars().enumerate() {
+                grid[r].push(col);
+            }
+        }
+        Grid { grid }
+    }
+
+    pub fn iter_cells(&self) -> impl Iterator<Item = GridCell> {
+        self.grid.iter().enumerate().flat_map(|(r, row)| {
+            row.iter().enumerate().map(move |(c, &value)| GridCell {
+                value,
+                row: r,
+                col: c,
+            })
+        })
+    }
+
+    pub fn count_neighbors_at<P>(&self, r: usize, c: usize, predicate: P) -> usize
+    where
+        P: Fn(char) -> bool,
+    {
+        let neighbors = [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ];
+
+        let mut count = 0;
+        for (nr, nc) in neighbors.iter() {
+            if let Some(neighbor_row) = self.grid.get((r as i32 + nr) as usize) {
+                if let Some(neighbor) = neighbor_row.get((c as i32 + nc) as usize) {
+                    if predicate(*neighbor) {
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        count
+    }
+
+    pub fn update_cells(&mut self, cells: Vec<GridCell>, new_value: char) -> i32 {
+        let mut updated = 0;
+        for cell in cells {
+            self.grid[cell.row][cell.col] = new_value;
+            updated += 1;
+        }
+        updated
+    }
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for row in &self.grid {
+            for col in row {
+                write!(f, "{}", col)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
